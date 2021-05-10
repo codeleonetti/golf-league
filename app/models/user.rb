cc9fb8d2eda921f73139a9f920912c2e1e9a5b65
john <john.d.leonetti@gmail.com>
2021-05-10 12:27:59 -0700

    user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-
+    
+    
+    has_secure_password
 
     validates :name, presence: true
     validates :username, presence: true, legnth: {minimum:5, maximum:15}, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+    10.times do
+      :name
+      :address
+      :username
+      :email
+      :password_digest
+
+    end
+
+
+puts " finished "
