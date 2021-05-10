68e607bdeccfd0eb0d7c5ce036c04e52384d013e
john <john.d.leonetti@gmail.com>
2021-05-10 10:20:30 -0700

    added rails resource for user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+    before_action :logged_in_user, only: [:edit, :update]
+
+    def index
+        @user = User.all
+    end
+
+    def show
+        find_user
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
+    private
+
+    def find_user
+        @user = User.find_by_id(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+
+end
