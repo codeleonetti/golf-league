19f19e394e0419488a3baeb151bcf39fc3b9c888
john <john.d.leonetti@gmail.com>
2021-05-26 13:21:40 -0700

    deleted unneeded has many

--- app/models/golf_course.rb
+++ app/models/golf_course.rb
@@ -2,13 +2,7 @@ class GolfCourse < ApplicationRecord
    
    has_many :teetimes
    has_many :users, through: :teetimes
-   
-   
-   
-   
-   
-    has_many :score_cards
-    has_many :users, :through => :score_cards
+
 
     
 
